763bdd1f9dcba70dc4db1d5c55933609e9b690a5
Rubén Zagon <ruben.zagon@gmail.com>
2022-03-24 08:10:51 +0000

    🧪 Add use cases in the to do list

--- src/test/java/com/rubenzagon/invoiceFilter/integration/CsvInvoiceFilterShould.java
+++ src/test/java/com/rubenzagon/invoiceFilter/integration/CsvInvoiceFilterShould.java
@@ -7,6 +7,14 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/*
+Requirimientos de negocio:
+ - Es válido que algunos campos estén vacíos (apareciendo dos comas seguidas o una coma final)
+ - El número de factura no puede estar repetido. Si lo estuviese eliminaríamos todas las líneas con repetición.
+ - Los impuestos IVA e IGIC son excluyentes, sólo puede aplicarse uno de los dos. Si alguna línea tiene contenido en ambos campos debe quedarse fuera.
+ - Los campos CIF y NIF son excluyentes, sólo puede usar uno de ellos.
+ - El neto es el resultado de aplicar al bruto el correspondiente impuesto. Si algún neto no está bien calculado se queda fuera.
+ */
 final class CsvInvoiceFilterShould {
 
     @Test
@@ -18,4 +26,13 @@ final class CsvInvoiceFilterShould {
 
         assertThat(result).isEqualTo(List.of(headerLine,invoiceLine));
     }
+
+    /*
+        - Un fichero con una sola factura donde IVA e IGIC están rellenos, deberían eliminar la línea
+        - Un fichero con una sola factura donde el neto está mal calculado, debería ser eliminada
+        - Un fichero con una sola factura donde CIF y NIF están rellenos, deberían eliminar la línea
+        - Un fichero con una sola línea es incorrecto porque no tiene cabecera
+        - Si el número de factura se repite en varias líneas, se eliminan todas ellas (Sin dejar ninguna).
+        - Una lista vacía o nula producirá una lista vacía de salida
+     */
 }
